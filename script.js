39fef1de7a6541aec53df181ca4077454ff9cc71
Amadi Jerry <amadijerry310@gmail.com>
2022-10-02 22:06:36 +0100

    testing layout and added light mode darkmode feature

---
+++ script.js
@@ -0,0 +1,21 @@
+const bodyTheme = document.querySelector(".body-bg")
+const selectTheme = document.querySelector(".theme")
+const todoBackgroundImage = document.querySelector(".todo-img")
+const lightTheme = document.querySelector(".light-theme")
+const darkTheme = document.querySelector(".dark-theme")
+
+lightTheme.addEventListener("click", () => {
+    lightTheme.style.display = "none"
+    darkTheme.style.display = "block"
+    bodyTheme.classList.add("body-bg2")
+    todoBackgroundImage.classList.add("todo-img2")
+    console.log("light mode activated")
+})
+darkTheme.addEventListener("click", () => {
+    darkTheme.style.display = "none"
+    lightTheme.style.display = "block"
+    bodyTheme.classList.remove("body-bg2")
+    todoBackgroundImage.classList.remove("todo-img2")
+    console.log("dark-mode activated")
+})
+
